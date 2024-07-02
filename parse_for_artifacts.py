5d42fda48aa90af5ab0894669d7f6752161e914b
Leo Cheng <leocheng@Leos-MBP.fios-router.home>
2024-07-02 06:46:25 -0400

    fixing python issue

--- parse_for_artifacts.py
+++ parse_for_artifacts.py
@@ -22,7 +22,7 @@ def main():
     f = open(f"{args.file_name}.json")
     data = json.load(f)
     for element in data['artifacts']:
-        if element['workflow_run'].get('id') == int(id) and os.path.splitext(element['name'])[1] != '.yml':
+        if element['workflow_run'].get('id') == int(args.action_run) and os.path.splitext(element['name'])[1] != '.yml':
             os.system(f"echo \"link: {str(element['url'])}\"")
 
             os.system(f"GHA_TOKEN={os.environ['GHA_TOKEN']} bash download-artifacts.sh "
